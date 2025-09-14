2228eab274139be50787827444331dbab8014ff9
john0 <maybemedic2@gmail.com>
2025-09-14 12:18:01 -0500

    Start of 2025-2026 hardware class and made color sensor test active.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Hardware2026.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware2026 {
+    public HardwareMap hwMap;
+
+    //motors
+    public DcMotorEx wheelFrontRight = null;
+    public DcMotorEx wheelFrontLeft = null;
+    public DcMotorEx wheelBackRight = null;
+    public DcMotorEx wheelBackLeft = null;
+
+    public DcMotorEx intake = null;
+
+}
