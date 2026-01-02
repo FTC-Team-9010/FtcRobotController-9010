5adb5cf1260955ff28bf230c3be1406db51de7af
FTC-Team-9010 <teamftc9010@gmail.com>
2026-01-02 17:34:49 -0600

    Added practice bot code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeDriver.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwarePractice;
+import org.firstinspires.ftc.teamcode.hardware.MecanumWheels2023;
+
+@TeleOp(name="PracticeDriver", group="TeleOps")
+public class PracticeDriver extends LinearOpMode {
+
+    HardwarePractice hdw;
+
+    MecanumWheels2023 robotWheel;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        hdw = new HardwarePractice(hardwareMap, telemetry); //init hardware
+        hdw.createHardware();
+        robotWheel = new MecanumWheels2023();
+
+        telemetry.addData("[>]", "All set?");
+        telemetry.update();
+
+        waitForStart();
+        telemetry.clearAll();
+
+        double turbo = 1;
+
+        Gamepad currentGamePad1 = new Gamepad();
+        Gamepad previousGamePad1 = new Gamepad();
+
+        currentGamePad1.copy(gamepad1);
+
+        //This is the main loop of operation.
+        while (opModeIsActive()) {
+
+            previousGamePad1.copy(currentGamePad1);
+            currentGamePad1.copy(gamepad1);
+
+            //Wheel takes input of gampad 1  ,  turbo is the power factor. Range 0-1 , 1 is 100%
+
+            robotWheel.joystick(gamepad1, turbo);
+
+            hdw.wheelFrontLeft.setVelocity(robotWheel.wheelFrontLeftPower * HardwarePractice.ANGULAR_RATE);
+            hdw.wheelBackLeft.setVelocity(robotWheel.wheelBackLeftPower * HardwarePractice.ANGULAR_RATE);
+            hdw.wheelFrontRight.setVelocity(robotWheel.wheelFrontRightPower * HardwarePractice.ANGULAR_RATE);
+            hdw.wheelBackRight.setVelocity(robotWheel.wheelBackRightPower * HardwarePractice.ANGULAR_RATE);
+
+            if (!previousGamePad1.a && currentGamePad1.a) {
+                if ( hdw.getLauncherPower()!=0 ) {
+                    hdw.launcherOff();
+                } else {
+                    hdw.launcherOn();
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwarePractice.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class HardwarePractice {
+    public HardwareMap hwMap;
+
+    public Telemetry telemetry;
+
+    public HardwarePractice(HardwareMap m, Telemetry tm) {
+        hwMap = m;
+        telemetry = tm;
+    }
+
+    static public double ANGULAR_RATE = 2500.0;
+    private final double MIN_VELOCITY = 0.1;
+
+    private float launcherPower = 1;
+
+    public DcMotorEx wheelFrontRight = null;
+    public DcMotorEx wheelFrontLeft = null;
+    public DcMotorEx wheelBackRight = null;
+    public DcMotorEx wheelBackLeft = null;
+
+    public DcMotorEx launcher = null;
+
+    public void createHardware() {
+
+        wheelFrontRight = hwMap.get(DcMotorEx.class, "rfWheel");
+        wheelFrontLeft = hwMap.get(DcMotorEx.class, "lfWheel");
+        wheelBackRight = hwMap.get(DcMotorEx.class, "rrWheel");
+        wheelBackLeft = hwMap.get(DcMotorEx.class, "lrWheel");
+
+        wheelFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        wheelFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        wheelBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        wheelFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        wheelBackRight.setDirection(DcMotor.Direction.FORWARD);
+
+        launcher = hwMap.get(DcMotorEx.class, "launcher");
+        launcher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        launcher.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        launcher.setVelocity(0);
+
+        wheelFrontRight.setVelocity(0);
+        wheelBackRight.setVelocity(0);
+        wheelFrontLeft.setVelocity(0);
+        wheelBackLeft.setVelocity(0);
+
+    }
+
+    public void launcherOn() {
+        launcher.setPower(launcherPower);
+    }
+
+    public void launcherOff() {
+        launcher.setPower(0);
+    }
+
+    public float getLauncherPower() {
+        return launcherPower;
+    }
+}
